25f915cdbf177e639d633a22606896dee6f6b914
AntumDeluge <antumdeluge@gmail.com>
2016-12-14 14:12:47 -0800

    globals.paths: Add PATH_home & PATH_config strings

--- source/globals/paths.py
+++ source/globals/paths.py
@@ -11,3 +11,5 @@ import os
 
 PATH_globals = os.path.dirname(__file__)
 PATH_root = os.path.dirname(PATH_globals)
+PATH_home = os.getenv(u'HOME')
+PATH_config = u'{}/.config/desktop_recorder'.format(PATH_home)
