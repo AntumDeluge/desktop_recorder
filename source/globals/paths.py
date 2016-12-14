7c84ccb8cda7bad0ece51a39cf047f181fa99835
AntumDeluge <antumdeluge@gmail.com>
2016-12-14 13:43:54 -0800

    New module: globals.paths: Defines locations of app & sub-modules

---
+++ source/globals/paths.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+
+## \package globals.paths
+
+# MIT licensing
+# See: LICENSE.txt
+
+
+import os
+
+
+PATH_globals = os.path.dirname(__file__)
+PATH_root = os.path.dirname(PATH_globals)
