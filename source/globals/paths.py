7686db996cf7929056d1ca2d915047943569d857
AntumDeluge <antumdeluge@gmail.com>
2016-12-14 14:59:12 -0800

    globals.paths: Path changes:
    Rename 'PATH_config' to 'PATH_confdir'.
    Set 'FILE_config' to absolute config file filename.
    Organize paths & files into groups.

--- source/globals/paths.py
+++ source/globals/paths.py
@@ -9,7 +9,13 @@
 import os
 
 
+# Application paths
 PATH_globals = os.path.dirname(__file__)
 PATH_root = os.path.dirname(PATH_globals)
+
+# System paths
 PATH_home = os.getenv(u'HOME')
-PATH_config = u'{}/.config/desktop_recorder'.format(PATH_home)
+PATH_confdir = u'{}/.config/desktop_recorder'.format(PATH_home)
+
+# System files
+FILE_config = u'{}/config'.format(PATH_confdir)
