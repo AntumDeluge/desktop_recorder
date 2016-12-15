7d73c868dc76eac3dca28a5d21b0deb464adf32b
AntumDeluge <antumdeluge@gmail.com>
2016-12-14 22:30:02 -0800

    ui.options: Remove unneeded background panel

--- source/ui/options.py
+++ source/ui/options.py
@@ -45,67 +45,65 @@ class Options(wx.Dialog):
         
         self.config = {}
         
-        self.pnl_bg = wx.Panel(self)
-        
-        chk_video = wx.CheckBox(self.pnl_bg, label=u'Include Video', name=u'video')
+        chk_video = wx.CheckBox(self, label=u'Include Video', name=u'video')
         chk_video.default = True
         
-        sel_vcodec = wx.Choice(self.pnl_bg, choices=vcodecs, name=u'vcodec')
+        sel_vcodec = wx.Choice(self, choices=vcodecs, name=u'vcodec')
         sel_vcodec.default = u'libtheora'
         
-        ti_quality = wx.TextCtrl(self.pnl_bg, name=u'quality')
+        ti_quality = wx.TextCtrl(self, name=u'quality')
         ti_quality.default = u'-1'
         
-        sel_framerate = wx.Choice(self.pnl_bg, choices=framerates, name=u'framerate')
+        sel_framerate = wx.Choice(self, choices=framerates, name=u'framerate')
         sel_framerate.default = u'30'
         
-        txt_vcontainer = wx.StaticText(self.pnl_bg, label=u'Container')
-        sel_vcontainer = wx.Choice(self.pnl_bg, choices=vcontainers, name=u'container')
+        txt_vcontainer = wx.StaticText(self, label=u'Container')
+        sel_vcontainer = wx.Choice(self, choices=vcontainers, name=u'container')
         sel_vcontainer.default = u'avi'
         
-        chk_audio = wx.CheckBox(self.pnl_bg, label=u'Include Audio', name=u'audio')
+        chk_audio = wx.CheckBox(self, label=u'Include Audio', name=u'audio')
         chk_audio.default = True
         
-        sel_acodec = wx.Choice(self.pnl_bg, choices=acodecs, name=u'acodec')
+        sel_acodec = wx.Choice(self, choices=acodecs, name=u'acodec')
         sel_acodec.default = u'libmp3lame'
         
-        spin_channels = wx.SpinCtrl(self.pnl_bg, name=u'channels')
+        spin_channels = wx.SpinCtrl(self, name=u'channels')
         spin_channels.default = 1
         
-        sel_samplerate = wx.Choice(self.pnl_bg, choices=samplerates, name=u'samplerate')
+        sel_samplerate = wx.Choice(self, choices=samplerates, name=u'samplerate')
         sel_samplerate.default = u'44100'
         
-        sel_bitrate = wx.Choice(self.pnl_bg, choices=bitrates, name=u'bitrate')
+        sel_bitrate = wx.Choice(self, choices=bitrates, name=u'bitrate')
         sel_bitrate.default = u'128k'
         
-        txt_filename = wx.StaticText(self.pnl_bg, label=u'Filename')
-        ti_filename = wx.TextCtrl(self.pnl_bg, name=u'filename')
+        txt_filename = wx.StaticText(self, label=u'Filename')
+        ti_filename = wx.TextCtrl(self, name=u'filename')
         ti_filename.default = u'out'
         
-        btn_target = wx.Button(self.pnl_bg, label=u'Folder')
-        self.ti_target = wx.TextCtrl(self.pnl_bg, name=u'dest')
+        btn_target = wx.Button(self, label=u'Folder')
+        self.ti_target = wx.TextCtrl(self, name=u'dest')
         self.ti_target.default = u'{}/Videos'.format(PATH_home)
         
         # *** Layout *** #
         
         vcodec_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        vcodec_sizer.Add(wx.StaticText(self.pnl_bg, label=u'Video Codec'), 0, wx.ALIGN_CENTER)
+        vcodec_sizer.Add(wx.StaticText(self, label=u'Video Codec'), 0, wx.ALIGN_CENTER)
         vcodec_sizer.Add(sel_vcodec, 0)
         
         qual_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        qual_sizer.Add(wx.StaticText(self.pnl_bg, label=u'Quality'), 0, wx.ALIGN_CENTER)
+        qual_sizer.Add(wx.StaticText(self, label=u'Quality'), 0, wx.ALIGN_CENTER)
         qual_sizer.Add(ti_quality, 1, wx.EXPAND)
         
         frate_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        frate_sizer.Add(wx.StaticText(self.pnl_bg, label=u'Framerate'), 0, wx.ALIGN_CENTER)
+        frate_sizer.Add(wx.StaticText(self, label=u'Framerate'), 0, wx.ALIGN_CENTER)
         frate_sizer.Add(sel_framerate, 0)
-        frate_sizer.Add(wx.StaticText(self.pnl_bg, label=u'FPS'), 0, wx.ALIGN_CENTER)
+        frate_sizer.Add(wx.StaticText(self, label=u'FPS'), 0, wx.ALIGN_CENTER)
         
         vcont_sizer = wx.BoxSizer(wx.HORIZONTAL)
         vcont_sizer.Add(txt_vcontainer, 0, wx.ALIGN_CENTER)
         vcont_sizer.Add(sel_vcontainer, 1, wx.EXPAND)
         
-        vidbox = wx.StaticBox(self.pnl_bg, label=u'Video')
+        vidbox = wx.StaticBox(self, label=u'Video')
         vidbox_sizer = wx.StaticBoxSizer(vidbox, wx.VERTICAL)
         vidbox_sizer.Add(chk_video, 0)
         vidbox_sizer.Add(vcodec_sizer, 0)
@@ -114,23 +112,23 @@ class Options(wx.Dialog):
         vidbox_sizer.Add(vcont_sizer, 0)
         
         acodec_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        acodec_sizer.Add(wx.StaticText(self.pnl_bg, label=u'Audio Codec'), 0, wx.ALIGN_CENTER)
+        acodec_sizer.Add(wx.StaticText(self, label=u'Audio Codec'), 0, wx.ALIGN_CENTER)
         acodec_sizer.Add(sel_acodec, 0)
         
         chan_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        chan_sizer.Add(wx.StaticText(self.pnl_bg, label=u'Channels'), 0, wx.ALIGN_CENTER)
+        chan_sizer.Add(wx.StaticText(self, label=u'Channels'), 0, wx.ALIGN_CENTER)
         chan_sizer.Add(spin_channels, 0)
         
         samp_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        samp_sizer.Add(wx.StaticText(self.pnl_bg, label=u'Samplerate'), 0, wx.ALIGN_CENTER)
+        samp_sizer.Add(wx.StaticText(self, label=u'Samplerate'), 0, wx.ALIGN_CENTER)
         samp_sizer.Add(sel_samplerate, 0)
-        samp_sizer.Add(wx.StaticText(self.pnl_bg, label=u'Hz'), 0, wx.ALIGN_CENTER)
+        samp_sizer.Add(wx.StaticText(self, label=u'Hz'), 0, wx.ALIGN_CENTER)
         
         br_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        br_sizer.Add(wx.StaticText(self.pnl_bg, label=u'Bitrate'), 0, wx.ALIGN_CENTER)
+        br_sizer.Add(wx.StaticText(self, label=u'Bitrate'), 0, wx.ALIGN_CENTER)
         br_sizer.Add(sel_bitrate, 0)
         
-        audbox = wx.StaticBox(self.pnl_bg, label=u'Audio')
+        audbox = wx.StaticBox(self, label=u'Audio')
         audbox_sizer = wx.StaticBoxSizer(audbox, wx.VERTICAL)
         audbox_sizer.Add(chk_audio, 0)
         audbox_sizer.Add(acodec_sizer, 0)
@@ -146,7 +144,7 @@ class Options(wx.Dialog):
         fold_sizer.Add(btn_target, 0, wx.ALIGN_CENTER)
         fold_sizer.Add(self.ti_target, 1, wx.EXPAND)
         
-        misc_box = wx.StaticBox(self.pnl_bg, label=u'Misc')
+        misc_box = wx.StaticBox(self, label=u'Misc')
         misc_sizer = wx.StaticBoxSizer(misc_box, wx.VERTICAL)
         misc_sizer.Add(fname_sizer, 0, wx.EXPAND)
         misc_sizer.Add(fold_sizer, 0, wx.EXPAND)
@@ -156,9 +154,9 @@ class Options(wx.Dialog):
         main_sizer.Add(audbox_sizer, 0, wx.EXPAND)
         main_sizer.Add(misc_sizer, 0, wx.EXPAND)
         
-        self.pnl_bg.SetAutoLayout(True)
-        self.pnl_bg.SetSizer(main_sizer)
-        self.pnl_bg.Layout()
+        self.SetAutoLayout(True)
+        self.SetSizer(main_sizer)
+        self.Layout()
         
         # *** Event handlers *** #
         
@@ -177,7 +175,7 @@ class Options(wx.Dialog):
     ## TODO: Doxygen
     def OnShow(self, event=None):
         if self.IsShown():
-            for C in self.pnl_bg.GetChildren():
+            for C in self.GetChildren():
                 c_name = C.GetName()
                 
                 if c_name in self.config:
@@ -197,7 +195,7 @@ class Options(wx.Dialog):
         
         else:
             # Set & write config when window is hidden
-            for C in self.pnl_bg.GetChildren():
+            for C in self.GetChildren():
                 c_name = C.GetName()
                 
                 if isinstance(C, wx.TextCtrl):
@@ -308,7 +306,7 @@ class Options(wx.Dialog):
             )
         
         opts_list = []
-        for C in self.pnl_bg.GetChildren():
+        for C in self.GetChildren():
             if isinstance(C, usable_types):
                 opts_list.append(u'{}={}'.format(C.GetName(), C.default))
         
