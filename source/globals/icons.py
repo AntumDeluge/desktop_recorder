86fc1d19f446752961e1f1debdb55ab6a015f6db
AntumDeluge <antumdeluge@gmail.com>
2016-12-15 20:38:00 -0800

    globals.icons: Changes to icon functions
    GetImage:
    - If width or height arguments are greater than 0, the image will be
    resized.
    GetBitmap:
    - New function.
    - Calls GetImage & converts to wx.Bitmap.
    GetIcon:
    - Calls GetBitmap & converts to wx.Icon. This allows icons to be
    resized.

--- source/globals/icons.py
+++ source/globals/icons.py
@@ -11,29 +11,49 @@ import os, wx
 from globals.paths import PATH_icons
 
 
+## Sets up a wx.Bitmap for use
+#  
+#  \param basename
+#    \b \e string : Name of image file without file extension (must be PNG)
+def GetBitmap(basename, width=-1, height=-1):
+    image = GetImage(basename, width, height)
+    
+    return image.ConvertToBitmap()
+
+
 ## Sets up a wx.Icon for use
 #  
 #  \param basename
 #    \b \e string : Name of image file without file extension (must be PNG)
-def GetIcon(basename):
-    image_file = u'{}/{}.png'.format(PATH_icons, basename)
-    if not os.path.isfile(image_file):
-        print(u'Warning: Could not locale image file: {}'.format(image_file))
-        
-        return None
+def GetIcon(basename, width=-1, height=-1):
+    bitmap = GetBitmap(basename, width, height)
     
-    return wx.Icon(image_file, wx.BITMAP_TYPE_PNG)
+    icon = wx.NullIcon
+    icon.CopyFromBitmap(bitmap)
+    
+    return icon
 
 
 ## Sets up a wx.Image for use
 #  
+#  Uses 'icons' directory to locate images
+#  
 #  \param basename
 #    \b \e string : Name of image file without file extension (must be PNG)
-def GetImage(basename):
+#  \param width
+#    \b \e int : Rescales image if greater than 0
+#  \param height
+#    \b \e int : Rescales image if greater than 0
+def GetImage(basename, width=-1, height=-1):
     image_file = u'{}/{}.png'.format(PATH_icons, basename)
     if not os.path.isfile(image_file):
-        print(u'Warning: Could not locale image file: {}'.format(image_file))
+        print(u'Warning: Could not locate image file: {}'.format(image_file))
         
         return None
     
-    return wx.Image(image_file, wx.BITMAP_TYPE_PNG)
+    image = wx.Image(image_file, wx.BITMAP_TYPE_PNG)
+    
+    if width > 0 or height > 0:
+        image.Rescale(width, height, wx.IMAGE_QUALITY_HIGH)
+    
+    return image
