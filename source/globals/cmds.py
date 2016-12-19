293a35cd6247ce0b036b8d3ab16f76f1e825f756
AntumDeluge <antumdeluge@gmail.com>
2016-12-19 07:02:06 -0800

    New module: globals.cmds:
    Defines GetCMD function for finding system commands.

---
+++ source/globals/cmds.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+## \package globals.cmds
+#  
+#  Retrieves & defines paths to commands that this app uses
+
+# MIT licensing
+# See: LICENSE.txt
+
+
+import os
+from subprocess     import PIPE
+from subprocess     import Popen
+from subprocess     import STDOUT
+
+from globals.debug  import pDebug
+
+
+def GetCMD(cmdname):
+    output, returncode = Popen((u'which', cmdname,), stdout=PIPE, stderr=STDOUT).communicate()
+    
+    if not returncode and os.access(output, os.X_OK):
+        return output
+    
+    pDebug(u'Failed to find command: {}'.format(cmdname))
