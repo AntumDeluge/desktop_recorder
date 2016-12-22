77f99f4862ded1b8493b5895e4f9d88a3bbf722b
AntumDeluge <antumdeluge@gmail.com>
2016-12-22 15:16:20 -0800

    Add function FieldDisabled to test for disabled controls

--- source/globals/fieldtests.py
+++ source/globals/fieldtests.py
@@ -9,17 +9,31 @@
 import wx
 
 
-## Tests if a wx control/instance is enabled
+## Tests if a wx control/instance is enabled/disabled
 #  
 #  Function for compatibility between wx versions
+#  \param field
+#    \b \e wx.Window : the wx control to check
 #  \param enabled
 #    \b \e bool : Check if enabled or disabled
+#  \return
+#    \b \e bool : True if field's enabled status is same as 'enabled'
 def FieldEnabled(field, enabled=True):
     if wx.MAJOR_VERSION > 2:
         return field.IsThisEnabled() == enabled
     
     else:
         return field.IsEnabled() == enabled
+
+
+## Tests if a wx control/instance is disabled
+#  
+#  \param field
+#    \b \e wx.Window : The wx field to check
+#  \return
+#    \b \e : True if field is disabled
+def FieldDisabled(field):
+    return FieldEnabled(field, False)
 
 
 ## Tests multiple fields
