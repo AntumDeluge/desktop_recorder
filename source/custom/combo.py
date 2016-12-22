50f3fad5b23ecedd9acb85e78633a6d4adb6d552
AntumDeluge <antumdeluge@gmail.com>
2016-12-22 11:49:59 -0800

    Add custom.combo.ComboBox:
    A ComboBox control derived from wx.combo.OwnerDrawnComboBox.

---
+++ source/custom/combo.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+## \package custom.combo
+
+# MIT licensing
+# See: LICENSE.txt
+
+
+import wx
+from wx.combo import OwnerDrawnComboBox
+
+
+class ComboBox(OwnerDrawnComboBox):
+    def __init__(self, parent, win_id, value=wx.EmptyString, pos=wx.DefaultPosition,
+            size=wx.DefaultSize, choices=[], style=0, validator=wx.DefaultValidator,
+            name=wx.ComboBoxNameStr):
+        OwnerDrawnComboBox.__init__(self, parent, win_id, value, pos, size, choices,
+                style, validator, name)
+        
+        self.Default = self.GetLabel()
+        self.Priority = []
+    
+    
+    ## Resets ComboBox to defaults
+    def Reset(self):
+        if not self.Count:
+            self.SetValue(self.Default)
+            return self.Value == self.Default
+        
+        return False
