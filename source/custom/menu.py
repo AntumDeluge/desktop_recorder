75a57edd2cf9007cf515ba656ca2754e38d3d1dc
AntumDeluge <antumdeluge@gmail.com>
2016-12-22 02:08:36 -0800

    Add custom.menu module:
    Adds custom Menu class that can retrieve a menu item's index.

---
+++ source/custom/menu.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+## \package custom.menu
+
+# MIT licensing
+# See: LICENSE.txt
+
+
+import wx
+
+
+class Menu(wx.Menu):
+    def __init__(self):
+        wx.Menu.__init__(self)
+    
+    
+    def GetIdIndex(self, mi_id):
+        index = -1
+        for MI in self.GetMenuItems():
+            index += 1
+            if MI.GetId() == mi_id:
+                break
+        
+        if index < 0:
+            return None
+        
+        return index
